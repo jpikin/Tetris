714cac3816d89f0f66c00851b220056f191325d7
Pikin <pikin@bk.ru>
2025-05-13 16:34:45 +0500

    FigureFactory added, KeyboardObserver added

---
+++ src/main/java/FigureFactory.java
@@ -0,0 +1,35 @@
+public class FigureFactory {
+
+    public static final int[][][] BRICKS = {{
+            {1, 1, 0},                          //   X X
+            {0, 1, 1},                          //     X X
+            {0, 0, 0}}, {                       //
+
+            {1, 0, 0},                          //   X
+            {1, 1, 0},                          //   X X
+            {0, 1, 0}}, {                       //     X
+
+            {0, 1, 0},                          //   X
+            {0, 1, 0},                          //   X
+            {0, 0, 0}}, {                       //   X
+
+            {1, 1, 0},                          //   X X
+            {1, 1, 0},                          //   X X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {0, 1, 0},                          //     X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {1, 1, 1},                          //   X X X
+            {0, 0, 0}}                          //
+    };
+
+
+
+    static Figure createRandomFigure(int x,int y){
+        int index = (int) (Math.random() * 6);
+        return new Figure(x, y, BRICKS[index]);
+    }
+}
--- src/main/java/Tetris.java
+++ src/main/java/Tetris.java
@@ -1,9 +1,15 @@
+import java.awt.event.KeyEvent;
+
+
 public class Tetris {
 
     private Field field;
     private Figure figure;
+    private boolean isGameOver;
 
     static Tetris game;
+
+
 
     public Field getField() {
         return field;
@@ -17,11 +23,45 @@ public class Tetris {
 
     }
 
-    void run() {
+    void run() throws Exception {
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
 
+        //выставляем начальное значение переменной "игра окончена" в ЛОЖЬ
+        isGameOver = false;
+        //создаем первую фигурку посередине сверху: x - половина ширины, y - 0.
+        figure = FigureFactory.createRandomFigure(field.getWidth() / 2, 0);
+
+        //пока игра не окончена
+        while (!isGameOver) {
+            //"наблюдатель" содержит события о нажатии клавиш?
+            if (keyboardObserver.hasKeyEvents()) {
+                //получить самое первое событие из очереди
+                KeyEvent event = keyboardObserver.getEventFromTop();
+                //Если равно символу 'q' - выйти из игры.
+                if (event.getKeyChar() == 'q') return;
+                //Если "стрелка влево" - сдвинуть фигурку влево
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    figure.left();
+                    //Если "стрелка вправо" - сдвинуть фигурку вправо
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    figure.right();
+                    //Если  код клавиши равен 12 ("цифра 5 на доп. клавиатуре") - повернуть фигурку
+                else if (event.getKeyCode() == 12)
+                    figure.rotate();
+                    //Если "пробел" - фигурка падает вниз на максимум
+                else if (event.getKeyCode() == KeyEvent.VK_SPACE)
+                    figure.downMaximum();
+            }
+
+            step();             //делаем очередной шаг
+            field.print();      //печатаем состояние "поля"
+            Thread.sleep(300);}
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         game = new Tetris();
         game.run();
     }
+
 }
